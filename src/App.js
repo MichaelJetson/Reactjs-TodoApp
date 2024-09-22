0ae3320ab14d2efcf2fff9ae54dc6307f335a106
MichaelJetson <michaeljet1402@gmail.com>
2024-09-22 22:34:46 +0530

    intial ToDo App setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { ToDoApp } from './components/todos/todo';
+
+function App() {
+  return (
+    <div className="App flex items-center justify-center bg-blue-500 min-h-screen ">
+      <ToDoApp />
+    </div>
+  );
+}
+
+export default App;
